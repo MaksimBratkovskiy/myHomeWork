35d2d5452accdbc3d18feeecb8b7b909fd1d3998
MaksimBratkovskiy <128903559+MaksimBratkovskiy@users.noreply.github.com>
2023-04-05 10:52:35 +0300

    Update ArrayStorage.java
    Исправлены баги

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,68 +1,79 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[10];
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null){
-                break;
-            }
-            if (storage[i] != null){
-                storage[i] = null;
-            }
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
         }
     }
 
     void save(Resume r) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null){
-                storage[i] = r;
-            }
-        }
+        storage[size()] = r;
+        //работает
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i].uuid.intern() == uuid.intern()){
-                return storage[i];
-            }
+        int index = posFoundVal(uuid);
+        if (index != -1){
+            return storage[index];
         }
         return null;
+
+        //работает
     }
 
+    //Удаляет найденый найденный элемент репозитория
     void delete(String uuid) {
-        int count = size();
-
-        for (int i = count - 1; i < storage.length - 1; i++) {
+        int posDelVal = posFoundVal(uuid);
+        storage[posDelVal] = null;
+        for (int i = posDelVal; i < storage.length - 1; i++) {
             storage[i] = storage[i + 1];
         }
+        //починил
+
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+
+    //Возвращает используемый репощзиторий
     Resume[] getAll() {
         //determine the size of the current storage
-        int count = size();
 
         //create intermediate storage and fill it
-        Resume[] currentStorage = new Resume[count];
-        for (int i = 0; i < currentStorage.length; i++) {
-            currentStorage[i] = storage[i];
-        }
-        return storage;
+
+        return Arrays.copyOf(storage, size());
+
+        // работает
     }
 
+    //находит размер используемого репозитория
     int size() {
         int count = 0;
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] == null){
-                count = i;
-                break;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null){
+                count++;
             }
         }
         return count;
+//        работает
+    }
+
+    //Находит позицию элемента (линейный поиск)
+    public int posFoundVal(String a) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(a)) {
+                return i;
+            }
+        }
+        return -1;
+//        работает
     }
 }
